55726938e225cbab0b8c8cc6126188db33c2453b
samuel <gyasisamuelkobina22@gmail.com>
2023-11-25 08:00:23 +0100

    hosting

--- registration.php
+++ registration.php
@@ -1,9 +1,51 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Check if the keys exist in the $_POST array before accessing them
+    $firstName = isset($_POST["firstName"]) ? $_POST["firstName"] : "";
+    $lastName = isset($_POST["lastName"]) ? $_POST["lastName"] : "";
+    $phoneNumber = isset($_POST["phoneNumber"]) ? $_POST["phoneNumber"] : "";
+    $email = isset($_POST["email"]) ? $_POST["email"] : "";
+    $country = isset($_POST["country"]) ? $_POST["country"] : "";
+    $passportNumber = isset($_POST["passportNumber"]) ? $_POST["passportNumber"] : "";
+    $jobTitle = isset($_POST["jobTitle"]) ? $_POST["jobTitle"] : "";
+    $organization = isset($_POST["organization"]) ? $_POST["organization"] : "";
+    $organizationCountry = isset($_POST["organizationCountry"]) ? $_POST["organizationCountry"] : "";
+
+    $to = "samuel.creatiftech@gmail.com"; // Replace with your email address
+    $subject = "New Form Submission";
+
+    $message = "hello I am $firstName from $country and I would like to register for 
+    the conference and below are my details\n";
+    $message .= "First Name: $firstName\n";
+    $message .= "Last Name: $lastName\n";
+    $message .= "Phone Number: $phoneNumber\n";
+    $message .= "Email: $email\n";
+    $message .= "Country: $country\n";
+    $message .= "Passport Number: $passportNumber\n";
+    $message .= "Job Title: $jobTitle\n";
+    $message .= "Entity/Organisation: $organization\n";
+    $message .= "Organization Country: $organizationCountry\n";
+
+    $headers = "From: Africamedform.com"; // 
+
+    // Attempt to send the email
+    if (mail($to, $subject, $message, $headers)) {
+        echo 'Registration  is successfully done.Our team will get in touch with you!';
+    } else {
+        // Display an error message on the form page
+        echo 'Registration failed. Kindly get in touch with us by Contact@africamedforum.com..';
+    }
+}
+?>
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
     <meta charset="utf-8">
-    <title>AfricaMed Kardev Website</title>
+    <title>AfricaMed Forum</title>
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
     <meta content="" name="keywords">
     <meta content="" name="description">
@@ -169,7 +211,8 @@
     <div class="container text-dark">
         <div class="row justify-content-center">
             <div class="col-md-8">
-                <form id="myForm" action="process_form.php" method="POST">
+                <form id="myForm" action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST">
+
 
                     <!-- Form fields go here -->
                     <div class="mb-3">
